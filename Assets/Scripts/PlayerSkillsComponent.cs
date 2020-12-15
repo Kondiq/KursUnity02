580048ff1362d869778520d5c0cb990877685a36
Konrad Szczygieł <kondiq.pl@gmail.com>
2020-12-15 18:47:46 +0100

    Start

---
+++ Assets/Scripts/PlayerSkillsComponent.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSkillsComponent : MonoBehaviour
+{
+    public int strenght;
+    public int agility;
+    public int intelligence;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
